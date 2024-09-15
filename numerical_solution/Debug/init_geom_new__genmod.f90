c3207187b93ca0b5b0864f40cb9504732342314e
IslamZ <143954875+ZakievI@users.noreply.github.com>
2024-09-15 14:00:56 +0300

    add: sort and create program file

---
+++ numerical_solution/Debug/init_geom_new__genmod.f90
@@ -0,0 +1,10 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Sun Sep 15 13:56:35 2024
+        MODULE INIT_GEOM_NEW__genmod
+          INTERFACE 
+            SUBROUTINE INIT_GEOM_NEW(G,H1,L1)
+              INTEGER(KIND=4) :: G
+              REAL(KIND=8) :: H1
+              REAL(KIND=8) :: L1
+            END SUBROUTINE INIT_GEOM_NEW
+          END INTERFACE 
+        END MODULE INIT_GEOM_NEW__genmod
